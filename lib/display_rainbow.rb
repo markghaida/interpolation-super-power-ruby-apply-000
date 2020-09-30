88139cb3dca0fab1b7185608fb477e457eca9e9a
Mark G <markghaida@gmail.com>
2020-09-30 00:42:50 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,4 +4,4 @@ def display_rainbow(colors)
  puts "#{colors}"
 end
 
-colors = ["red","orange","yellow","green","blue","indigo","violet"]
+colors = ['red','orange','yellow','green','blue','indigo','violet']
